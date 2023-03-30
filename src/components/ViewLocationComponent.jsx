e57bca3eb0f8a0313788877de824ffb82b32a3ed
king-archy <king.archy.13013@gmail.com>
2023-03-29 19:53:30 -0700

    view works

--- src/components/ViewLocationComponent.jsx
+++ src/components/ViewLocationComponent.jsx
@@ -1,54 +1,47 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import LocationService from "../services/LocationService";
+import { useParams } from "react-router-dom";
 
-class ViewLocationComponent extends Component {
-  constructor(props) {
-    super(props);
+const ViewLocationComponent = () => {
+  const { id } = useParams();
+  const [location, setLocation] = useState({});
 
-    this.state = {
-      id: this.props.match.params.id,
-      location: {},
-    };
-  }
-
-  componentDidMount() {
-    LocationService.getLocationById(this.state.id).then((res) => {
-      this.setState({ location: res.data });
+  useEffect(() => {
+    LocationService.getLocationById(id).then((res) => {
+      setLocation(res.data);
     });
-  }
+  }, [id]);
 
-  render() {
-    return (
-      <div>
-        <br></br>
-        <div className="card col-md-6 offset-md-3">
-          <h3 className="text-center"> View Location Details</h3>
-          <div className="card-body">
-            <div className="row">
-              <label> Astronomer: </label>
-              <div> {this.state.location.astronomer}</div>
-            </div>
-            <div className="row">
-              <label> Location: </label>
-              <div> {this.state.location.location}</div>
-            </div>
-            <div className="row">
-              <label> GPS Coordinates: </label>
-              <div> {this.state.location.gps}</div>
-            </div>
-            <div className="row">
-              <label> Image URL: </label>
-              <div> {this.state.location.imageUrl}</div>
-            </div>
-            <div className="row">
-              <label> Description: </label>
-              <div> {this.state.location.description}</div>
-            </div>
+  return (
+    <div>
+      <br></br>
+      <div className="card col-md-6 offset-md-3">
+        <h3 className="text-center"> View Location Details</h3>
+        <div className="card-body">
+          <div className="row">
+            <label> Astronomer: </label>
+            <div> {location.astronomer}</div>
+          </div>
+          <div className="row">
+            <label> Location: </label>
+            <div> {location.location}</div>
+          </div>
+          <div className="row">
+            <label> GPS Coordinates: </label>
+            <div> {location.gps}</div>
+          </div>
+          <div className="row">
+            <label> Image URL: </label>
+            <div> {location.imageUrl}</div>
+          </div>
+          <div className="row">
+            <label> Description: </label>
+            <div> {location.description}</div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ViewLocationComponent;
